728d8fafc3fcf784b87f485d7c30ec8df47c34da
eformat <eformat@gmail.com>
2015-07-17 19:58:33 +1200

    updated

--- bin/go.sh
+++ bin/go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)/..
+DIR=`pwd`
 
 . environment
 
@@ -10,12 +11,14 @@ if ! which java &>/dev/null; then
   exit 1
 fi
 
-bin/cache.sh
+[ "$USER" -ne "root" ]; exit "please run as root on ose-master"
 
-sudo htpasswd -b /etc/openshift/openshift-passwd $DEMOUSER $DEMOPW
+$DIR/bin/cache.sh
+
+htpasswd -b /etc/openshift/openshift-passwd $DEMOUSER $DEMOPW
 
 for img in $STI_IMAGESTREAMS; do
-  sudo oc create -n openshift -f - <<EOF
+  oc create -n openshift -f - <<EOF
 kind: ImageStream
 apiVersion: v1
 metadata:
@@ -27,28 +30,37 @@ spec:
 EOF
 done
 
-su - devops
-oc login $OSEARGS -u $DEMOUSER -p $DEMOPW
-
 for proj in $INTEGRATION $DEMOUSER; do
-  oc new-project $proj
+  oadm new-project $proj --display-name="$proj" --description="CICD $proj" --admin=$DEMOUSER
  
-  for repo in $INTEGRATION_REPOS; do
-    monster/$repo/deploy.sh
-    [ -e monster/$repo/build.sh ] && monster/$repo/build.sh
+  for repo in $INTEGRATION_REPOS; do  	
+  	su $DEMOUSER <<EOF
+		oc login -u $DEMOUSER -p $DEMOPW $OSEARGS
+		oc project $proj
+		$DIR/monster/$repo/deploy.sh  	
+EOF
+
+  	su $DEMOUSER <<EOF
+  		oc login -u $DEMOUSER -p $DEMOPW $OSEARGS
+  		oc project $proj
+   		[ -e $DIR/monster/$repo/build.sh ] && $DIR/monster/$repo/build.sh
+EOF
+   		
   done
 done
 
 for proj in $PROD; do
-  oc new-project $proj
+  oadm new-project $proj --display-name="$proj" --description="CICD $proj" --admin=$DEMOUSER
 
   for repo in $PROD_REPOS; do
-    monster/$repo/deploy.sh
+		oc login -u $DEMOUSER -p $DEMOPW $OSEARGS
+		oc project $proj
+		$DIR/monster/$repo/deploy.sh
   done
 done
 
 for proj in $INFRA; do
-  oc new-project $proj
+  oadm new-project $proj --admin=$DEMOUSER
 
   for repo in $INFRA_REPOS; do
     infra/$repo/deploy.sh
@@ -56,5 +68,5 @@ for proj in $INFRA; do
 done
 
 # serviceAccount required for containers running as root
-#echo '{"kind": "ServiceAccount", "apiVersion": "v1", "metadata": {"name": "root"}}' | sudo oc create -n infra -f -
-#(sudo oc get -o yaml scc privileged; echo - system:serviceaccount:infra:root) | sudo oc update scc privileged -f -
+echo '{"kind": "ServiceAccount", "apiVersion": "v1", "metadata": {"name": "root"}}' | sudo oc create -n infra -f -
+(sudo oc get -o yaml scc privileged; echo - system:serviceaccount:infra:root) | sudo oc update scc privileged -f -
